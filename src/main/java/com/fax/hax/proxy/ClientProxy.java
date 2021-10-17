68c4389f67ea27b353da13051245e6ccc9d9a7f6
playper3 <saper6569@gmail.com>
2021-10-16 19:31:19 -0700

    Client Proxy class

---
+++ src/main/java/com/fax/hax/proxy/ClientProxy.java
@@ -0,0 +1,34 @@
+package com.fax.hax.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
+
+public class ClientProxy extends ServerProxy {
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+    }
+
+    @Override
+    public void serverStarting(FMLServerStartingEvent event) {
+        super.serverStarting(event);
+    }
+
+    @Override
+    public void serverStopping(FMLServerStoppingEvent event) {
+        super.serverStopping(event);
+    }
+}
