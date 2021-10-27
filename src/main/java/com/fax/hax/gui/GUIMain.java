da126afd02092e95e9452571c511bc67eed78ee2
playper3 <saper6569@gmail.com>
2021-10-26 17:21:47 -0700

    Gui's main class

--- src/main/java/com/fax/hax/gui/GUIMain.java
+++ src/main/java/com/fax/hax/gui/GUIMain.java
@@ -1,4 +1,11 @@
 package com.fax.hax.gui;
 
+import javax.swing.*;
+
 public class GUIMain {
+    JFrame frame = new JFrame("Click_Gui");
+
+    public void createScreen() {
+        frame.setSize(12, 12);
+    }
 }
